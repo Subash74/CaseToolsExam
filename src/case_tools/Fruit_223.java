0207335074b60af92e1f81a7c4bed9ca9e9c93a3
prince <prince@DESKTOP-18294MS>
2021-07-23 14:17:21 +0530

    added get_color method

--- src/case_tools/Fruit_223.java
+++ src/case_tools/Fruit_223.java
@@ -2,9 +2,13 @@ package case_tools;
 
 public class Fruit_223 {
 	String color;
-	Fruit(String color)
+	Fruit_223(String color)
 	{
 	this.color=color;
+	}
+	String get_color()
+	{
+	return color;
 	}
 }
 
